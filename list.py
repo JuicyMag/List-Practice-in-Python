86a4d1b1fa484e5e8a802f281018c512b0e849ed
Magnus Herweyer <magnus.herweyer@gmail.com>
2022-08-08 13:55:24 -0500

    adds ceiling and floor func

--- list.py
+++ list.py
@@ -28,11 +28,39 @@ def is_odd(list):
     for element in list:
         if element %2 !=0:
             odd_list.append(element)
-    return odd_list   
+    return odd_list 
+
+odd_list = [x for x in test_even_function if x%2 !=0]
+# print(odd_list)  
+
+#func consumes a list and a returns a list of all numbers greater than floor
+def is_greater_than_specified(list, floor):
+    floor_list = []
+    for num in list:
+        if num > floor:
+            floor_list.append(num)
+    return floor_list
+
+
+
+#func consumes a list and a returns a list of all numbers less than ceiling
+def is_less_than_specified(list, ceiling):
+    ceiling_list = []
+    for num in list:
+        if num < ceiling:
+            ceiling_list.append(num)
+    return ceiling_list
+
+b = is_less_than_specified(test_even_function, 5)
+print(b)
+
+# a = is_greater_than_specified(test_even_function, 5)
+# print(a)
+
 
 # generate_num_list(1000)
 # print(generate_num_list)
 # a = is_even(test_even_function)
 # print(a)
-b = is_odd(test_even_function)
-print(b)
+# b = is_odd(test_even_function)
+# print(b)
